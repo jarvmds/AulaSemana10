73e5bd9daa46f80cfa1e995df00c7cd0b3301cb9
jarvmds <jessicaavarella@hotmail.com>
2022-05-15 16:59:39 -0300

    adição do Lab no banco de dados

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿//Console.WriteLine(args);
-
-//foreach (var arg in args)
-//{
-//  Console.WriteLine(arg);
-//} 
-// digitar "dotnet run -- 67 ola 'shrek' true"
-
-using Microsoft.Data.Sqlite;
+﻿using Microsoft.Data.Sqlite;
 
 var connection = new SqliteConnection("Data Source=database.db");
 
@@ -69,6 +61,74 @@ if(modelName == "Computer")
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
         command.Parameters.AddWithValue("$processor", processor);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+
+
+
+
+connection.Open();
+
+var command = connection.CreateCommand();
+command.CommandText = @"
+    CREATE TABLE IF NOT EXISTS Labs(
+        id int not null primary key,
+        number varchar(100) not null,
+        name varchar(100) not null,
+        block varchar(100) not null
+
+    );
+";
+
+command.ExecuteNonQuery();
+
+connection.Close();
+
+    if(modelName == "Lab")
+{
+    if(modelAction == "List")
+    {
+        Console.WriteLine("Lab List");
+
+        connection = new SqliteConnection("Data Source=database.db");
+
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Labs;";
+
+        var reader = command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            Console.WriteLine("id {0}, number {1}, name {2}, block {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+        }
+
+
+        reader.Close();
+        connection.Close();
+    }
+
+    if(modelAction == "New")
+    {
+        int id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        var name = args[4];
+        var block = args[5];
+
+       
+        connection = new SqliteConnection("Data Source=database.db");
+
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Computers VALUES($id, $number, $name, $block);";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
 
         command.ExecuteNonQuery();
         connection.Close();
