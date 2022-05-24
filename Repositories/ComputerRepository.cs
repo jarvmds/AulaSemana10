91632baf90ad06f0635205e7df44d15e2b187582
jarvmds <jessicaavarella@hotmail.com>
2022-05-24 17:23:45 -0300

    refactor: change computer repository save method

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -33,7 +33,7 @@ class ComputerRepository
         return computers;
     }
 
-    public void Save (Computer computer)
+    public Computer Save (Computer computer)
     {
         var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
@@ -46,6 +46,7 @@ class ComputerRepository
 
         command.ExecuteNonQuery();
         connection.Close();
+        return computer;
     }
     
     
