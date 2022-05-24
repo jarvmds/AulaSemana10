46d16e266249168eea44046fa7f79b307f803dd3
Jéssica Raya <95510461+jarvmds@users.noreply.github.com>
2022-05-24 08:33:32 -0300

    Update ComputerRepository.cs

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -1,30 +1,34 @@
 using LabManager.Models;
+using LabManager.Database;
 using Microsoft.Data.Sqlite;
 
 namespace LabManager.Repositories;
 
 class ComputerRepository
 {
+    private DatabaseConfig databaseConfig;
+    
+    public ComputerRepository(DatabaseConfig databaseConfig) => this.databaseConfig = databaseConfig;
     public List<Computer> GetAll()
     {
-        var computers = new List<Computer>();
-
+        var computers  = new List<Computer>();
+        
         var connection = new SqliteConnection("Data Source=database.db");
-
         connection.Open();
-
+        
         var command = connection.CreateCommand();
         command.CommandText = "SELECT * FROM Computers;";
-
+        
         var reader = command.ExecuteReader();
 
         while(reader.Read())
         {
-            var computer = new Computer(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+            var computer = new Computer(reader.GetInt32(0),reader.GetString(1),reader.GetString(2));
+
             computers.Add(computer);
         }
-
         connection.Close();
+
         return computers;
     }
 }
